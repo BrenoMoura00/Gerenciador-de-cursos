eea902ab368b382bae05e878d4b0d39c08da4641
BrenoMoura00 <brenoluisadv@gmail.com>
2024-04-13 22:35:40 -0300

    iniciando o projeto

---
+++ Gerenciador-de-cursos/src/br/com/curso/testandoListas.java
@@ -0,0 +1,11 @@
+package br.com.curso;
+
+public class testandoListas {
+    public static void main(String[] args) {
+        String aula1 = "Conhecendo mais de listas";
+        String aula2 = "Modelando a Classe Aula";
+        String aula3 = "Trabalhando com cursos e Sets";
+        String aula4 = "Aumentando a complexidade";
+        
+    }
+}
